a1e5ce9f99cb9851adb1f88f9a77bb478fde020e
Iasmim Saraiva de Sousa <iasmimsousasaraiva11@gmail.com>
2026-08-12 19:20:54 -0300

    feat: initialize Spring Boot library API

---
+++ src/main/java/com/saraiva/biblioteca/entity/Author.java
@@ -0,0 +1,48 @@
+package com.saraiva.biblioteca.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "author")
+    @JsonIgnore
+    List<Book> books;
+
+    public Author(){
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
---
+++ src/main/java/com/saraiva/biblioteca/entity/Book.java
@@ -0,0 +1,67 @@
+package com.saraiva.biblioteca.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    @Column(name = "publication_year")
+    private Integer publicationYear;
+
+    @Column(name = "is_read")
+    private Boolean isRead;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book(){
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public Boolean getRead() {
+        return isRead;
+    }
+
+    public void setRead(Boolean read) {
+        isRead = read;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
